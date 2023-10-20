c12b5597212a1b9661c75f7e5d26d13d2d2f2239
Kolia321 <kolyagura569@ukr.net>
2023-10-20 19:42:59 +0300

    Set up project

---
+++ src/pages/Signup.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react'
+import { Link,useNavigate } from 'react-router-dom'
+import {UserAuth} from '../context/AuthContext'
+
+const Signup = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');    
+    const {user, signUp} = UserAuth()
+    const navigate = useNavigate()
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        try{
+            await signUp(email, password);
+            navigate('/')
+        }catch(error){
+            console.log(error);
+        }
+    }
+
+
+
+  return (
+    <>
+    <div className='w-full h-screen '>
+        <img className='hidden sm:block absolute w-full h-full object-cover ' src="https://assets.nflxext.com/ffe/siteui/vlv3/ab180a27-b661-44d7-a6d9-940cb32f2f4a/299d22b9-1b9f-4bf3-a31f-45f494bcbe90/UA-uk-20231009-popsignuptwoweeks-perspective_alpha_website_large.jpg" alt="/" />
+        <div className='bg-black/60 fixed top-0 left-0 w-full h-screen'>
+            <div className='fixed w-full px-4 py-24 z-50 '></div>
+            <div className='max-w-[450px] h-[600px] mx-auto bg-black/75 text-white '>
+            <div className='max-w-[320px] mx-auto py-16'>
+            <h1 className='text-3xl font-bold '>Sign Up</h1>
+            <form onSubmit={handleSubmit} className='w-full flex flex-col py-4 '>
+                <input onChange={(e) => setEmail(e.target.value)} className='p-3 my-2 bg-gray-700 rounded' type='email' placeholder='Email or phone number ' autoComplete='email' />
+                <input onChange={(e) => setPassword(e.target.value)} className='p-3 my-2 bg-gray-700 rounded' type="password" placeholder='Password' autoComplete='current-password' />
+                <button className='bg-red-600 py-3 my-6 rounded font-bold'>Sign Up</button>
+                <div className='flex justify-between items-center tetx-sm text-gray-600'>
+                    <p><input className='mr-2' type="checkbox"/>Remember me</p>
+                    <p>Need help?</p>
+
+                </div>
+                <p className='py-4'><span className='text-gray-600'>Already subscribed to Netflix?</span>{' '}
+                <Link to='/login'>
+                </Link>    
+                 Sign In</p>
+            </form>
+            </div>
+            </div>
+
+        </div>
+    </div>
+    </>
+
+  )
+}
+
+export default Signup
